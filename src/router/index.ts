4f86ef132aa795e5983f863eb6a85594561ed93e
SMazeikis <salvijus.mazeikis2@mail.dcu.ie>
2021-02-19 22:03:59 -0800

    names

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import SignUp from '../views/SignUp.vue';
-import SignIn from '../views/LogIn.vue';
+import SignIn from '../views/SignIn.vue';
 import ActualHome from '../views/ActualHome.vue';
 import ForgotPassword from '../views/ForgotPassword.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'signup',
+    name: 'signUp',
     component: SignUp
   },
   {
